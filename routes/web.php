7cfd5f0fcce5e828ad3ce7d1daff99462ec12878
Rinfella <rinfellapachuau93@gmail.com>
2022-10-12 21:38:16 +0530

    web.php modified ani

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
- Route::resource('areas',AreaController::class);
+Route::resource('areas',AreaController::class);
